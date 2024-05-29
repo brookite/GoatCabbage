da24797bc815f80354411ac3dca96bf93c68e7b2
brookite <brookit18@gmail.com>
2024-05-29 23:03:59 +0300

    Added debug method for generating paddock from string

--- src/main/java/brookite/games/goatcabbage/model/utils/Debug.java
+++ src/main/java/brookite/games/goatcabbage/model/utils/Debug.java
@@ -5,6 +5,57 @@ import brookite.games.goatcabbage.model.Paddock;
 import brookite.games.goatcabbage.model.entities.*;
 
 public class Debug {
+    public static Paddock paddockFromString(String s) {
+        String[] rows = s.split("\n");
+        int columns = rows[0].length();
+        for (String row : rows) {
+            columns = Math.max(columns, row.length());
+        }
+        Paddock pd = new Paddock(columns, rows.length);
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < rows[i].length(); j++) {
+                switch (rows[i].charAt(j)) {
+                    case '@':
+                        pd.cell(i + 1, j + 1).putEntity(new Goat(30));
+                        break;
+                    case '#':
+                        pd.cell(i + 1, j + 1).putEntity(new Wall());
+                        break;
+                    case '$':
+                        pd.cell(i + 1, j + 1).putEntity(new SimpleBox());
+                        break;
+                    case 'M':
+                        pd.cell(i + 1, j + 1).putEntity(new MetalBox());
+                        break;
+                    case 'U':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.WEST, true));
+                        break;
+                    case '<':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.NORTH, true));
+                        break;
+                    case '>':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.SOUTH, false));
+                        break;
+                    case ')':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.NORTH, false));
+                        break;
+                    case '(':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.SOUTH, true));
+                        break;
+                    case '^':
+                        pd.cell(i + 1, j + 1).putEntity(new MagnetBox(Direction.EAST, false));
+                        break;
+                    case '.':
+                        pd.cell(i + 1, j + 1).putEntity(new Cabbage());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return pd;
+    }
+
     public static String drawPaddock(Paddock pd) {
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i < pd.getHeight() + 1; i++) {
